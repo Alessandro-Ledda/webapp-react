c5fce59698e1cdb070ff43213d7ab4c7a41f5d6e
Alessandro Ledda <aleledda75@icloud.com>
2026-02-23 20:12:23 +0100

    feat in file HomePage.jsx/pages

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,21 @@
+import CardMovie from "../components/CardMovie";
+
+function HomePage() {
+    return (
+        <>
+            <h1>BOOL MOVIES</h1>
+            <h2>Movies List</h2>
+
+            <div className="home-container">
+                <div className="content home">
+                    <CardMovie />
+                    <CardMovie />
+                    <CardMovie />
+                    <CardMovie />
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default HomePage
