d775a3a9874b3eb13333a3e6f6272248ca407497
Alessandro Ledda <aleledda75@icloud.com>
2026-02-23 20:14:40 +0100

    feat route home page in file app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import DefaultLayout from "../layouts/DefaultLayout"
+import HomePage from "./pages/Homepage"
 
 function App() {
 
@@ -8,7 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route element={<DefaultLayout />}>
-
+            <Route index element={<HomePage />} />
           </Route>
         </Routes>
       </BrowserRouter>
