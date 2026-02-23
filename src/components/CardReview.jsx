1e4344fc2cddbd348571abf9129a6aa3602b43c2
Alessandro Ledda <aleledda75@icloud.com>
2026-02-23 20:22:41 +0100

    feat card recensione in file CardReview.jsx/components

---
+++ src/components/CardReview.jsx
@@ -0,0 +1,14 @@
+function CardReview() {
+
+    return (
+        <div className="card-review">
+            <div className="card-body">
+                <p className="card-text">testo recensione</p>
+                <strong>vote: 7</strong>
+                <address><i>nome autore</i></address>
+            </div>
+        </div>
+    )
+}
+
+export default CardReview
