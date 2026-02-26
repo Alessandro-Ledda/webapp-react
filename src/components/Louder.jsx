4c9ec92854045df3f2000a8647d817d0c2677d35
Alessandro Ledda <aleledda75@icloud.com>
2026-02-26 17:09:51 +0100

    feat Loader in file Loader.jsx

---
+++ src/components/Louder.jsx
@@ -0,0 +1,9 @@
+function Loader() {
+    return (
+        <div className="overlay">
+            <div className="spinner-border" role="status"></div>
+        </div>
+    )
+}
+
+export default Loader
