65eedc592ac4b2d6be27f37b4a69ef7e86cff925
Alessandro Ledda <aleledda75@icloud.com>
2026-02-26 17:36:16 +0100

    import + utilizzo del context in app.jsx + layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,23 @@ import DefaultLayout from "./layouts/DefaultLayout"
 import HomePage from "./pages/Homepage"
 import MoviePages from "./pages/MoviePage"
 import NotFoundPage from "./pages/NotFoundPage"
+import { GlobalProvider } from "./GlobalContext"
 
 function App() {
 
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<DefaultLayout />}>
-            <Route index element={<HomePage />} />
-            <Route path="/movies/:id" element={<MoviePages />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <GlobalProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<DefaultLayout />}>
+              <Route index element={<HomePage />} />
+              <Route path="/movies/:id" element={<MoviePages />} />
+              <Route path="*" element={<NotFoundPage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </GlobalProvider>
     </>
   )
 }
--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -2,13 +2,24 @@ import { Outlet } from "react-router-dom";
 
 import MainHeader from "../components/MainHeader"
 
+// import hook costum del contesto globale
+import { useGlobal } from "../GlobalContext";
+
+// import del loader
+import Loader from "../components/Louder";
+
 function DefaultLayout() {
+
+    // attivo l'utilizzo del/dei valore/i messi a disposizione del contesto globale
+    const { isLoading } = useGlobal();
+
     return (
         <>
             <MainHeader />
             <main className="container">
                 <Outlet />
             </main>
+            {isLoading && <Loader />}
         </>
     )
 }
